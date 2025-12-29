dae7208fca258357a9995a6baa9dd3fa1110fd3a
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2025-12-29 19:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //your JS code here. If required.
- const codes = document.querySelectorAll(".code");
+const codes = document.querySelectorAll(".code");
 
-  codes[0].focus();
+codes[0].focus();
 
-  codes.forEach((input, index) => {
-    input.addEventListener("input", (e) => {
-      if (e.target.value !== "" && index < codes.length - 1) {
-        codes[index + 1].focus();
-      }
-    });
-
-    input.addEventListener("keydown", (e) => {
-      if (e.key === "Backspace") {
-        if (input.value === "" && index > 0) {
-          codes[index - 1].focus();
-          codes[index - 1].value = "";
-        }
-      }
-    });
+codes.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    if (e.target.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
   });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = "";
+      }
+    }
+  });
+});
